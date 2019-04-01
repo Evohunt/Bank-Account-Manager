0051a7a95b400213f3800ca282ed2831cbcfbb06
Evohunt <andrei_dana97@yahoo.com>
2019-04-02 00:49:42 +0300

    Added Startup Screen + More CSS Files

--- src/main/java/Manager/model/Main.java
+++ src/main/java/Manager/model/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
         primaryStage.setTitle("Bank Account Manager");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
